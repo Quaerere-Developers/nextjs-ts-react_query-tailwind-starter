4f9b1b378e97928ac53a2704eb15a4c992aa9882
mk668a <msftoaa@gmail.com>
2021-06-18 15:48:17 +0900

    change proj name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 import Button from "@material-tailwind/react/Button";
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1 className="my-2">Hello umimori 👋</h1>
+    <h1 className="my-2">Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
         <a>About</a>
