99933961a01995d2fc0cf3c98cc0f64a33edb27e
mk668a <msftoaa@gmail.com>
2021-06-18 15:53:40 +0900

    delete material tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
-import Button from "@material-tailwind/react/Button";
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1 className="my-2">Hello Next.js 👋</h1>
@@ -9,7 +9,6 @@ const IndexPage = () => (
         <a>About</a>
       </Link>
     </p>
-    <Button>button</Button>
   </Layout>
 )
 
