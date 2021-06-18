fde1585319e4cdc409f3d1add0f872cdbc3f2dd2
mk668a <msftoaa@gmail.com>
2021-06-18 15:47:29 +0900

    setting tailwind

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import "@material-tailwind/react/tailwind.css";
+import 'tailwindcss/tailwind.css'
+import '../styles/globals.scss'
+import IndexPage from './index'
+
+const App = () => (
+  <IndexPage />
+)
+
+export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
-
+import Button from "@material-tailwind/react/Button";
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello umimori 👋</h1>
+    <h1 className="my-2">Hello umimori 👋</h1>
     <p>
       <Link href="/about">
         <a>About</a>
       </Link>
     </p>
+    <Button>button</Button>
   </Layout>
 )
 
